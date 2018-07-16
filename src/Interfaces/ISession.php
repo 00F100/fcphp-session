5c3c3e049397632502dbf514789bb74e0f383d21
Joao Moraes <joaomoraesbr@gmail.com>
2018-07-16 20:38:35 -0300

    add interface

--- src/Interfaces/ISession.php
+++ src/Interfaces/ISession.php
@@ -4,6 +4,36 @@ namespace FcPhp\Session\Interfaces
 {
 	interface ISession
 	{
-		
+		/**
+         * Method to construct instance of Session
+         *
+         * @param FcPhp\Cookie\Interfaces\ICookie $cookie Instance of Cookie
+         * @return void
+         */
+        public function __construct(ICookie $cookie);
+        
+        /**
+         * Method to set new information into Session
+         *
+         * @param string $key Key to information
+         * @param mixed $value Value to save into Key Session
+         * @return void
+         */
+        public function set(string $key, $value) :void;
+        
+        /**
+         * Method to publish into Cookie
+         *
+         * @return void
+         */
+        public function commit() :void;
+        
+        /**
+         * Method to get information from Session
+         *
+         * @param string $key Key to information
+         * @return mixed
+         */
+        public function get(string $key = null);
 	}
 }
